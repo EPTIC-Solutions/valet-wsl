5a76f90e26043be8ae5c6c97ab795ef02a6a9d49
Cristian Bilu <bilucristian25@gmail.com>
2022-03-24 03:04:19 +0200

    Update valet to latest version

--- cli/valet.php
+++ cli/valet.php
@@ -19,7 +19,7 @@ use Symfony\Component\Console\Question\ConfirmationQuestion;
  */
 Container::setInstance(new Container());
 
-$version = 'v1.0.3';
+$version = 'v1.0.6';
 
 $app = new Application('Valet WSL', $version);
 
